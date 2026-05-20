d23b93a117ae97aa166c62af23d219852a416cb6
Aulia Brahmantio Diaz <55686668+brahmantiodiaz@users.noreply.github.com>
2026-05-20 11:18:28 +0700

    feat: add get profile route

--- routes/userProfile.js
+++ routes/userProfile.js
@@ -2,6 +2,7 @@ const router = require("express").Router();
 const UserProfileController = require("../controllers/UserProfileController");
 const authentication = require("../middlewares/authentication");
 
+router.get("/profile", authentication, UserProfileController.getProfile);
 router.post("/profile", authentication, UserProfileController.createProfile);
 router.put("/profile", authentication, UserProfileController.updateProfile);
 
